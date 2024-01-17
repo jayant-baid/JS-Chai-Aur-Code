faf60678b4f7bd42142bacab7920d598be301133
Jayant Baid <baidjayant2002@gmail.com>
2024-01-17 17:19:01 +0530

    Added Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer js version
+// GOOD PRACTICE but aajkl yeh nhi likhne se bhi chl jata h because js automatically 
+
+// alert("Hello") // We are using nodejs, not browser
+
+//! Code readablility should be high
+let name="Jayant"
+let age=18
+let isLoggedIn=false
+
+// number => range: 2 to power 53
+// bigint -> trading, bigapp
+// string => ""
+// boolean => true/false
+// null => standalone value -> representation of empty -> khali h, undefined nhi h
+// undefined => jb value assign nhi hui h
+// symbol => uniqueness ke lie use hota h 
+
+// Object
+console.log(typeof null) // Object
+console.log(typeof undefined) // undefined
+// NaN
